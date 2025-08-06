9e55803d5df239bb3edbacbe683eba590c331150
Alejandro <alepd132@gmail.com>
2025-08-06 02:27:59 +0200

    En principio es funcional, pero falta arreglar el problema de la cantidad de espacios.

--- Foundations/JS - Calculator/script.js
+++ Foundations/JS - Calculator/script.js
@@ -6,9 +6,9 @@ document.querySelectorAll('.buttons > button[id]').forEach(btn => {
 //Operation Buttons
 let result = document.getElementById("display");
 let clearButton = document.getElementById("clear");
-let equalButton = document.getElementById("equal");
+let equalsButton = document.getElementById("equals");
 let addButton = document.getElementById("add");
-let subtractButton = document.getElementById("subtract");
+let substractButton = document.getElementById("substract");
 let multiplyButton = document.getElementById("multiply");
 let divideButton = document.getElementById("divide");
 let decimalButton = document.getElementById("decimal");
@@ -19,6 +19,7 @@ let numberButtons = [];
 for (let i = 0; i <= 9; i++) {
     numberButtons.push(document.getElementById(`number${i}`));
 }
+
 //Event Listeners for Number Buttons
 numberButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
@@ -27,29 +28,78 @@ numberButtons.forEach((button, index) => {
 });
 
 decimalButton.addEventListener("click", () => {
-    typeNumber(index);
+    if (result.innerText != "0")
+        typeNumber(".");
 });
 
 clearButton.addEventListener("click", () => {
     currentInput = "0";
+    result.innerText = currentInput;
+    operationButton = "";
+    storedValue = "";
+});
+
+addButton.addEventListener("click", () => {
+    if(storedValue == "")
+        storedValue = 0;
+    operationSign = "+";
+    callingOperate();
+});
+
+multiplyButton.addEventListener("click", () => {
+    if(storedValue == ""){
+        storedValue = 1;
+    }
+    operationSign = "*";
+    callingOperate();
+});
+
+divideButton.addEventListener("click", () => {
+    operationSign = "/";
+    if(storedValue == ""){
+        storedValue = Number(result.innerText) * Number(result.innerText)   ;
+    }
+    callingOperate();
+});
+
+substractButton.addEventListener("click", () => {
+    console.log("INICIO: Storedvalue vale " + storedValue + " y el display muestra " + result.innerText);
+    if(storedValue=="")
+        storedValue = Number(result.innerText) *2;
+    operationSign = "-";
+    callingOperate();
+    console.log("FIN: Storedvalue vale " + storedValue + " y el display muestra " + result.innerText);
+});
+
+equalsButton.addEventListener("click", () => {
+    if (operationSign != "") {
+        callingOperate();
+        operationSign = "";
+        storedValue = "";
+    }
 });
 
 backspaceButton.addEventListener("click", () => {
-    currentInput = currentInput - currentInput.charAt(currentInput.length);
+    if (currentInput.length > 1) {
+        currentInput = currentInput.slice(0, -1);
+    } else {
+        currentInput = "0";
+    }
+    result.innerText = currentInput;
 });
 
 //Calculator Variables
-let firstOperand = "";
-let secondOperand = "";
+let storedValue = "";
 let operationSign = "";
 let currentInput = "0";
+let booleanOperation = false;
 
 let addFunction = (a, b) => a + b;
 let substractFunction = (a, b) => a - b;
 let multiplyFunction = (a, b) => a * b;
 let divideFunction = (a, b) => a / b;
 
-let operate = (a, b, operation) => {
+let operate = (a, operation, b) => {
     switch (operation) {
         case "+":
             return addFunction(a, b);
@@ -65,16 +115,24 @@ let operate = (a, b, operation) => {
 };
 
 function typeNumber(number) {
+    if (booleanOperation == true) {
+        result.innerText = "";
+        currentInput = "";
+        booleanOperation = false;
+    }
     if (currentInput.length < 16) {
         if (currentInput == "0") {
-            currentInput = number;
-            console.log("currentInput vale 0");
+            currentInput = String(number);
         }
         else {
-            currentInput += number;
-            console.log("currentInput no vale 0");
+            currentInput += String(number);
         }
-        document.getElementById("display").innerText = currentInput;
+        result.innerText = currentInput;
     }
-    console.log(`current Input vale: ${currentInput}`)
+}
+
+function callingOperate() {
+    storedValue = operate(Number(storedValue), operationSign, Number(result.innerText));
+    booleanOperation = true;
+    result.innerText = String(storedValue)
 }
